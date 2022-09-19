cba21cd19561c85d6e7a65e18763c979665e7173
jingxiu <1693186883@qq.com>
2022-09-19 10:39:12 +0800

    使用协程处理字段

--- core/application.go
+++ core/application.go
@@ -21,7 +21,7 @@ func (a *Application) Overflow(arr []string) {
 		if strings.Contains(item, ".") {
 			arr := strings.Split(item, ".")
 			idx := int64(0)
-			roll(arr, &a.Parse, &idx)
+			roll(arr, a.Parse, &idx)
 		} else {
 			delete(a.Parse, item)
 		}
@@ -36,47 +36,62 @@ func parseStruct(obj interface{}) map[string]interface{} {
 	types := value.Type()
 	// 解析到Parse 字段
 	result := make(map[string]interface{})
+	ch := make(chan map[string]interface{})
+	defer close(ch)
+	go worker(ch, result)
+	wg := sync.WaitGroup{}
+	wg.Add(value.NumField())
 	for i := 0; i < value.NumField(); i++ {
-		v := value.Field(i)
-		tag := types.Field(i).Tag.Get(TAGNAME)
-		if strings.Contains(tag, ",") {
-			tag = strings.TrimSpace(strings.Split(tag, ",")[0])
-		}
-		switch v.Kind() {
-		case reflect.Ptr:
-			res := parseStruct(v.Interface())
-			result[tag] = res
-		case reflect.Slice:
-			if v.Index(0).Kind() != reflect.Struct && v.Index(0).Kind() != reflect.Ptr {
-				result[tag] = v.Interface()
-			} else {
-				wg := sync.WaitGroup{}
-				wg.Add(v.Len())
-				slice := make([]map[string]interface{}, 0)
-				for i := 0; i < v.Len(); i++ {
-					go func(item reflect.Value) {
-						res := parseStruct(item.Interface())
-						slice = append(slice, res)
-						wg.Done()
-					}(v.Index(i))
-				}
-				wg.Wait()
-				result[tag] = slice
+		go func(v reflect.Value, i int) {
+			//v := value.Field(i)
+			tag := types.Field(i).Tag.Get(TAGNAME)
+			if strings.Contains(tag, ",") {
+				tag = strings.TrimSpace(strings.Split(tag, ",")[0])
 			}
-		default:
-			result[tag] = v.Interface()
-		}
+			switch v.Kind() {
+			case reflect.Ptr:
+				res := parseStruct(v.Interface())
+				//result[tag] = res
+				ch <- map[string]interface{}{tag: res}
+			case reflect.Slice:
+				if !v.IsNil() && v.Index(0).Kind() != reflect.Struct && v.Index(0).Kind() != reflect.Ptr {
+					//result[tag] = v.Interface()
+					ch <- map[string]interface{}{tag: v.Interface()}
+				} else {
+					wg := sync.WaitGroup{}
+					wg.Add(v.Len())
+					slice := make([]map[string]interface{}, 0)
+					for i := 0; i < v.Len(); i++ {
+						go func(item reflect.Value) {
+							res := parseStruct(item.Interface())
+							slice = append(slice, res)
+							wg.Done()
+						}(v.Index(i))
+					}
+					wg.Wait()
+					//result[tag] = slice
+					ch <- map[string]interface{}{tag: slice}
+				}
+			default:
+				//result[tag] = v.Interface()
+				ch <- map[string]interface{}{tag: v.Interface()}
+			}
+
+			wg.Done()
+		}(value.Field(i), i)
 	}
+	wg.Wait()
+	ch <- nil
 	return result
 }
-func roll(arr []string, sc *map[string]interface{}, idx *int64) {
+func roll(arr []string, sc map[string]interface{}, idx *int64) {
 	if *idx < int64(len(arr)-1) {
-		d := *sc
+		d := sc
 		m := d[arr[*idx]].(map[string]interface{})
 		next := *idx + 1
-		roll(arr[*idx:], &m, &next)
+		roll(arr[*idx:], m, &next)
 	} else {
-		delete(*sc, arr[*idx])
+		delete(sc, arr[*idx])
 	}
 }
 
@@ -85,4 +100,19 @@ func correction(value reflect.Value) reflect.Value {
 		value = value.Elem()
 	}
 	return value
+}
+
+func worker(ch chan map[string]interface{}, result map[string]interface{}) {
+	for {
+		select {
+		case res, ok := <-ch:
+			if !ok {
+				//wg.Done()
+				return
+			}
+			for k, v := range res {
+				result[k] = v
+			}
+		}
+	}
 }
